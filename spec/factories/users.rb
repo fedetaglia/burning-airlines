96c792a57a205838336915dfc5e1fb696447cdf4
lukemesiti <lukemesiti@gmail.com>
2014-03-13 11:38:56 +1100

    configured golden path for flight policies

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryGirl.define do
         email "example@example.com"
         password "example123"
         password_confirmation "example123"
+        role "user"
+    end
+
+    factory :admin, parent: :user do
+        role "admin"
     end
 end
