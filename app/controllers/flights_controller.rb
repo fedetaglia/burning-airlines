e46c47123fc7d6e70c38af0e587418ce0fcea50e
lukemesiti <lukemesiti@gmail.com>
2014-03-13 11:50:46 +1100

    select current user in _header

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   before_action :set_flight, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
 
   # GET /flights
   # GET /flights.json
@@ -74,6 +75,10 @@ class FlightsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_flight
       @flight = Flight.find(params[:id])
+    end
+
+    def set_user
+      @user = current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
